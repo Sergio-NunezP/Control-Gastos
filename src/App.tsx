fbb47f088ab285ead0227b496d49fcf5a1c9c4e3
Sergio Núñez <sergioop722@gmail.com>
2025-02-06 17:03:41 -0500

    ventana modal y formulario para gastos

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import BudgetForm from "./components/BudgetForm"
 import { useMemo } from "react"
 import { useBudget } from "./hooks/useBudget"
 import BudgetTracker from "./components/BudgetTracker"
+import ExpenseModal from "./components/ExpenseModal"
 
 
 function App() {
@@ -21,6 +22,12 @@ function App() {
       <div className="max-w-3xl mx-auto bg-white shadow-lg rounded-lg mt-10 p-10">
         {isValidBudget ? <BudgetTracker /> : <BudgetForm />}
       </div>
+
+      <main className="max-w-3xl mx-auto py-10">
+        {isValidBudget && (
+          <ExpenseModal />
+        )}
+      </main>
     </>
   )
 }
--- src/reducers/budget-reducer.ts
+++ src/reducers/budget-reducer.ts
@@ -1,16 +1,20 @@
 
 // 1: Se define la acción
 export type BudgetActions =
-    { type: 'add-budget', payload: { budget: number } }
+    { type: 'add-budget', payload: { budget: number } } |
+    { type: 'show-modal' } |
+    { type: 'close-modal' }
 
 // 2: Se define el State
 export type BudgetState = {
     budget: number
+    modal: boolean
 }
 
 // 3: Se inicializa el State
 export const initialState: BudgetState = {
-    budget: 0
+    budget: 0,
+    modal: false
 }
 
 // 4: Se crea el Reducer
@@ -19,11 +23,26 @@ export const budgetReducer = (
     action: BudgetActions
 ) => {
 
-    if (action.type === 'add-budget')
+    if (action.type === 'add-budget') {
         return {
             ...state,
             budget: action.payload.budget
         }
+    }
+
+    if (action.type === 'show-modal') {
+        return {
+            ...state,
+            modal: true
+        }
+    }
+
+    if (action.type === 'close-modal') {
+        return {
+            ...state,
+            modal: false
+        }
+    }
 
     return state
 }
